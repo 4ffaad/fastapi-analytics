0e11d845cff763acf0c422c8f2b390d9ab35c0d3
Daffa <Daffafariq@gmail.com>
2025-07-10 01:48:31 +0800

    Implement docker compose and dockerfile.web

---
+++ README.md
@@ -0,0 +1,4 @@
+ 
+ ## Docker 
+-- ` docker build -t analytics-api -f Dockerfile.web . `
+-- ` docker run analytics-api ` 
--- boot/docker-run.sh
+++ boot/docker-run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Activate virtual environment
 source /opt/venv/bin/activate
 
+# Go to project directory
 cd /code
-RUN_PORT =${RUN_PORT:-8000}
-RUN_HOST =${RUN_HOST:-0.0.0.0}
 
-gunicorn -k uvicorn.workers.UvicornWorker -b ${RUN_HOST}:${RUN_PORT} main:app
+# Set default values
+RUN_PORT=${RUN_PORT:-8000}
+RUN_HOST=${RUN_HOST:-0.0.0.0}
+
+# Run FastAPI app with Gunicorn
+exec gunicorn -k uvicorn.workers.UvicornWorker -b ${RUN_HOST}:${RUN_PORT} main:app
