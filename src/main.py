12c395ee1c53d70e4e99b3f9efe41cbd6d30c512
Daffa <Daffafariq@gmail.com>
2025-07-11 21:28:59 +0800

    first api endpoint

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "Worlder"}
@@ -13,3 +14,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/healthz")
+def read_api_health():
+    return {"status": "ok"}
