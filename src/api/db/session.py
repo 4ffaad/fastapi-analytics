6e51e7c2e5a84e6311ae4dc3e1ee694dcb22a4a3
Daffa <Daffafariq@gmail.com>
2025-07-17 18:13:35 +0800

    fixed bugges and added date time field

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -5,7 +5,7 @@ from .config import DATABASE_URL
 if DATABASE_URL == "":
     raise NotImplementedError("DATABASE_URL is not set. Please configure it in your environment variables.")
 
-engine = sqlmodel.create_engine("sqlite:///./test.db", echo=True)
+engine = sqlmodel.create_engine(DATABASE_URL, echo=True)
 
 def init_db():
     print("Initializing database connection...")
--- src/api/events/models.py
+++ src/api/events/models.py
@@ -1,14 +1,24 @@
+from datetime import datetime, timezone
 from typing import List, Optional
 from sqlmodel import SQLModel, Field
+import sqlmodel 
+
 """
 id 
 path 
 description
 """
+
+def get_utc_now():
+    return datetime.now(timezone.utc)
 class EventModel (SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)   
     page: Optional[str] = ""
     description: Optional[str] = ""
+    created_at: datetime = Field(
+        default_factory=get_utc_now, 
+        sa_type=sqlmodel.DateTime(timezone=True),
+        nullable=False)
 
 class EventCreateSchema(SQLModel):
     page: str
