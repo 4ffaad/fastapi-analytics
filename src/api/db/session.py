00c15ffc1604aba345601ef31e703735b122c728
Daffa <Daffafariq@gmail.com>
2025-07-21 18:00:54 +0800

    Verifying queries are working and added data in the hypertables

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -6,7 +6,7 @@ import timescaledb
 if DATABASE_URL == "":
     raise NotImplementedError("DATABASE_URL is not set. Please configure it in your environment variables.")
 
-engine = sqlmodel.create_engine(DATABASE_URL, timezone=DB_TIMEZONE)
+engine = timescaledb.create_engine(DATABASE_URL, timezone=DB_TIMEZONE)
 
 def init_db():
     print("Initializing database connection...")
